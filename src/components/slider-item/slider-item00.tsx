f4df6db831cd345f0c5c27f4e7c8efd62aa230bc
가동재 <masterforce999@naver.com>
2026-03-26 12:26:41 +0900

    업데이트

--- src/components/slider-item/slider-item00.tsx
+++ src/components/slider-item/slider-item00.tsx
@@ -2,21 +2,28 @@ import Image from "next/image";
 
 const R2_URL = process.env.NEXT_PUBLIC_R2_PUBLIC_URL;
 
-const PREFIX: Record<string, string> = {
-  kr: "pk",
-  en: "pe",
+const PREFIX: Record<string, { pc: string; mobile: string }> = {
+  kr: { pc: "pk", mobile: "mk" },
+  en: { pc: "pe", mobile: "me" },
 };
 
 export default function SliderItem00({ locale }: { locale: string }) {
-  const prefix = PREFIX[locale] || PREFIX.ko;
+  const { pc, mobile } = PREFIX[locale] || PREFIX.ko;
 
   return (
     <div className="relative w-full h-full">
       <Image
-        src={`${R2_URL}/suwon/${prefix}_1.png`}
-        alt="수원1"
+        src={`${R2_URL}/suwon/${mobile}_1.png`}
+        alt="수원2"
         fill
-        className="object-cover"
+        className="object-cover min-[1080px]:hidden block"
+        priority
+      />
+      <Image
+        src={`${R2_URL}/suwon/${pc}_1.png`}
+        alt="수원2"
+        fill
+        className="object-cover hidden min-[1080px]:block"
         priority
       />
     </div>
