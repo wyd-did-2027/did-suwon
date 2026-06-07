a5f09995b7f78e1913a0542daa3149acf527a6a9
가동재 <masterforce999@naver.com>
2026-06-07 19:46:53 +0900

    Update OG share image

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -8,7 +8,7 @@ export function isLocale(value: string): value is Locale {
 export const siteConfig = {
   name: "2027 WYD 수원교구대회 공식 사이트",
   url: "https://www.wyd2027did-suwon.org",
-  ogImage: "/logo.png",
+  ogImage: "https://r2-image-server.masterforce999.workers.dev/opengraph-image.png",
 };
 
 export const content = {
